f6b471594380f7bffa7720444dffcdfc28eaf72a
alex <alex_kobilianskiy@bigmir.net>
2019-03-25 08:41:31 +0200

    section advantages complete

--- gulpfile.js
+++ gulpfile.js
@@ -47,6 +47,7 @@ gulp.task('scripts', function() {
 		'app/libs/waypoints/lib/jquery.waypoints.min.js',
 		'app/libs/animate-number/jquery.animateNumber.min.js',
 		'app/libs/formstyler/jquery.formstyler.min.js',
+		'app/libs/matchHeight/jquery.matchHeight-min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
